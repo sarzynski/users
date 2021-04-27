8d5ceee3936ed3ad5fed1ede2084caebce22e5fc
Kamil Sarzyński <kamil.s.sarzynski@gmail.com>
2021-04-27 21:52:52 +0200

    add form object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,53 +7,46 @@ class UsersController < ApplicationController
     @birthday_user = User.birthday_user
   end
 
-  # GET /users/1 or /users/1.json
   def show
     @user_presenter = UserPresenter.new(@user)
   end
 
-  # GET /users/new
   def new
-    @user = User.new
+    @user_form = UserForm.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users or /users.json
   def create
-    @user = User.new(user_params)
-      if @user.save
-        redirect_to @user, notice: "User was successfully created."
+    @user_form = UserForm.new(user_params)
+      if @user_form.save
+        redirect_to users_path, notice: "User was successfully created."
       else
         render :new, status: :unprocessable_entity
       end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
-      if @user.update(user_params)
-        redirect_to @user, notice: "User was successfully updated."
+      if @user_form.update(user_params)
+        redirect_to users_path, notice: "User was successfully updated."
       else
         render :edit, status: :unprocessable_entity
       end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
       redirect_to users_url, notice: "User was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :birth_date)
+      params.require(:user_form).permit(:first_name, :last_name, :email, :birth_date)
     end
 end
---
+++ app/forms/user_form.rb
@@ -0,0 +1,18 @@
+class UserForm
+  include ActiveModel::Model
+  include ActiveRecord::AttributeAssignment
+
+  attr_accessor :first_name, :last_name, :email, :birth_date
+
+  validates_presence_of :first_name, :last_name, :email, :birth_date
+
+  def save
+    if valid?
+      user = User.new(first_name: first_name, last_name: last_name, email: email, birth_date: birth_date)
+      user.save!
+      true
+    else
+      false
+    end
+  end
+end
