b59d5f025632e565a1221a86ae52aca8a7f00796
Kamil Sarzyński <kamil.s.sarzynski@gmail.com>
2021-04-27 22:01:02 +0200

    refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[show edit update destroy]
 
   def index
     @users = User.all
@@ -14,38 +14,37 @@ class UsersController < ApplicationController
     @user_form = UserForm.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @user_form = UserForm.new(user_params)
-      if @user_form.save
-        redirect_to users_path, notice: "User was successfully created."
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @user_form.save
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
-      if @user_form.update(user_params)
-        redirect_to users_path, notice: "User was successfully updated."
-      else
-        render :edit, status: :unprocessable_entity
-      end
+    if @user_form.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @user.destroy
-      redirect_to users_url, notice: "User was successfully destroyed."
+    redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-      params.require(:user_form).permit(:first_name, :last_name, :email, :birth_date)
-    end
+  def user_params
+    params.require(:user_form).permit(:first_name, :last_name, :email, :birth_date)
+  end
 end
